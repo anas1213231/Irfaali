98f766c2aabab9ebf0e29823a435feae8116a2a9
anas1213231 <anas2007250@gmail.com>
2026-09-16 22:11:41 +0400

    app: route Edit tab to Studio V2

--- Irfaali/App/RootView.swift
+++ Irfaali/App/RootView.swift
@@ -13,7 +13,7 @@ struct RootView: View {
     var body: some View {
         TabView(selection: $selectedTab) {
             NavigationStack {
-                StudioView()
+                StudioViewV2()
             }
             .opacity(selectedTab == .studio ? 1 : 0)
             .animation(
